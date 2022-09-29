c6dd091837953794f2c6d04502ce7f836f0671de
Nameera-Khatoon <nameerakhatoon.m@gmail.com>
2022-09-29 12:30:49 +0530

    navbar css and updated img

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,34 +4,31 @@ var nav=document.createElement("div") // nav-main-div
 var a_logo=document.createElement("a") // # current page left corner
 let left_div=document.createElement("div")// left tabs
 let right_div=document.createElement("div")//right corner
-
 let tabs=[ {"name":"WOMEN"},{"name":"MEN"},{"name":"BEAUTY"},{"name":"KIDS"},{"name":"HOME"},{"name":"DESIGNERS"},{"name":"COMMUNITY"},{"name":"WHY MODESENS"},{"name":"SALE"},] //child-left tab
-let span=[
-    { "img":"","span":"COUNTRIES & LANGUAGES"},
-    {"i":'<i class="fa-thin fa-bell"></i>',"span":"notification"},
-    {"span":"checkout"},
-    {"span":"avtar"},
-    {"span":"search"}
+let spans=[
+    { "src":"./Images/nav/unitedstate2.jpg","span":"COUNTRIES & LANGUAGES"},
+    {"src":"./Images/nav/icons8-notification-25.png","span":"notification"},
+    {"src":"./Images/nav/shopping-bag.jpg","span":"checkout"},
+    {"src":"./Images/nav/icons8-male-user-24.png","span":"avtar"},
+    {"src":"./Images/nav/search_icon.jpg","span":"search"}
 ]
 
 let i=document.createElement("i")
 // i.innerHTML="<i class='fa-thin fa-bell'></i>"
 console.log(i)
-span.forEach(function(el){
+spans.forEach(function(el){
     let div=document.createElement("div");
     let btn=document.createElement("button");
+    let img=document.createElement("img")
+    img.src=el.src
+    let span=document.createElement("span")
+    span.innerText=el.span
+    btn.append(img,span)
+    span.style.visibility="hidden"
+    document.createAttribute("class","hidden-span")
+    div.append(btn);
+    right_div.append(div)
 })
-
-
-
-
-
-
-
-
-
-
-let noti=document.createElement("div")
 
 
 let logoImg=document.createElement("img");
