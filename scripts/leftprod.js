6a9d9085dbc64b89354b1efede0b4d74cd8addf3
Nameera-Khatoon <nameerakhatoon.m@gmail.com>
2022-10-02 02:50:07 +0530

    filtering data

---
+++ scripts/leftprod.js
@@ -0,0 +1,29 @@
+let drop=["CATEGORY",
+    "DESIGNER",
+    "STOCK",
+    "COLOR",
+   "PRICE RANGE",
+    "ON SALE",
+    "STORE",
+    "KEYWORD"]
+let l_div=document.getElementById("left")
+for(let i=0;i<5;i++){
+    let divl=document.createElement("div")
+    console.log(divl)
+    let id1="divL"+i
+    console.log(id1)
+    divl.setAttribute("id",id1)
+    l_div.append(divl)
+}
+
+
+
+for(let i=0;i<drop.length;i++){
+let selct=document.createElement("select");
+let opt=document.createElement("option")
+opt.value=drop[i]
+selct.append(opt)
+let d=document.getElementById("divL1")
+console.log(d)
+d.append(selct)
+}
